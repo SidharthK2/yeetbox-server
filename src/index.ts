777c91f9ce9f38b16e858b61aa712687639fe0d3
SidharthK2 <sidharthkapoor.100@gmail.com>
2025-09-16 00:58:00 +0100

    Implement CORS middleware and log environment in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { health } from "./routes/health";
 import { file } from "./routes/file";
 import { cookieHelper } from "./middleware";
 
 const app = new Hono();
+console.log("Environment: ", Bun.env.NODE_ENV);
+
+const allowedOrigins = ["https://yeetbox.vercel.app"];
+
+app.use(
+	"/*",
+	cors({
+		origin: allowedOrigins,
+		allowMethods: ["GET", "POST", "PUT", "DELETE"],
+		allowHeaders: ["Content-Type"],
+	}),
+);
 
 app.use("/file/*", cookieHelper);
 
